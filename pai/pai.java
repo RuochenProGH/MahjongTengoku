52695a9933fc5b05108f49b93ea54a9d97081f2e
Ruochen <zczlrs2@ucl.ac.uk>
2022-01-31 07:32:26 +0000

    pai_interface

---
+++ pai/pai.java
@@ -0,0 +1,33 @@
+package pai;
+
+interface pai {
+    // index indicates the number of 萬筒索 or position of 字牌
+    int index = 0;
+
+    // muji　字牌
+    boolean muji = false;
+
+    // sangen　三元牌
+    boolean sangen = false;
+
+    // fu　風牌
+    boolean fu = false;
+
+    // oneNice 一九・老頭牌
+    boolean raotou = false;
+
+    // dora ドラ
+    boolean dora = false;
+
+    // su 数牌
+    boolean su = false;
+
+    // man 萬
+    boolean man = false;
+
+    // pin 筒
+    boolean pin = false;
+
+    // so 索
+    boolean so = false;
+}
---
+++ pai/paiList_classic.java
@@ -0,0 +1,11 @@
+package pai;
+
+import java.util.ArrayList;
+
+public enum paiList_classic {
+    WHITE, FA, ZHONG, // sangen: white, green, red 白發中
+    DONG, NAN, XI, BEI, // fu:　東南西北
+    MAN_0, MAN_1, MAN_2, MAN_3, MAN_4, MAN_5, MAN_6, MAN_7, MAN_8, // man　萬1-9
+    PIN_0, PIN_1, PIN_2, PIN_3, PIN_4, PIN_5, PIN_6, PIN_7, PIN_8, // pin　筒1-9
+    SO_0, SO_1, SO_2, SO_3, SO_4, SO_5, SO_6, SO_7, SO_8 // so 索1-9
+}
